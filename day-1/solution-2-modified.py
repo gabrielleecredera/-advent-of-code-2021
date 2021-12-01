22bf1f84733927ba70c700a260bcf039a65765d4
Gabriel Lee <gabriel.lee@credera.co.uk>
2021-12-01 05:21:25 +0000

    day 1 - modification

---
+++ day-1/solution-2-modified.py
@@ -0,0 +1,11 @@
+increases = 0
+last_depth_sum = 0
+
+depths = [int(depth) for depth in open("input-1.txt").read().splitlines()]
+
+for depths in zip(depths, depths[1:], depths[2:]):
+    if sum(depths) > last_depth_sum:
+        increases += 1
+    last_depth_sum = sum(depths)
+
+print(increases - 1)
