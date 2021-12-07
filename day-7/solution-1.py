1073c3af8a33510e479826eba2ea194cb5cbf3de
Gabriel Lee <gabriel.lee@credera.co.uk>
2021-12-07 05:16:21 +0000

    day 7

---
+++ day-7/solution-1.py
@@ -0,0 +1,16 @@
+import re
+from collections import Counter
+
+positions = Counter([int(x) for x in re.findall(r'(\d+)', open("input.txt").read())])
+best_target = 0
+best_cost = 9999999999999
+
+for target in range(max(positions) + 1):
+    cost = 0
+    for position, count in positions.items():
+        cost += count * abs(target - position)
+    if (cost < best_cost):
+        best_cost = cost
+        best_target = target
+
+print(best_cost)
